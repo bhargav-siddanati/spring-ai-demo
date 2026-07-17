c71fb221805ba79b7a8669be1173e7c251b09cce
bhargav-siddanati <bhargav77990@gmail.com>
2026-07-17 10:13:00 +0530

    Added Chatoptions

--- src/main/java/com/bhargav/ai/demo/controller/AIChatController.java
+++ src/main/java/com/bhargav/ai/demo/controller/AIChatController.java
@@ -1,10 +1,12 @@
 package com.bhargav.ai.demo.controller;
 
 import com.bhargav.ai.demo.service.AIChatService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("ai/api")
@@ -19,5 +21,14 @@ public class AIChatController {
     @GetMapping("/chat")
     public String chatWithAI(@RequestParam String message){
         return service.chatWithOllama(message);
+    }
+
+    @GetMapping("/chat-options")
+    public String chatWithAIOptions(@RequestParam String msg){
+        return service.chatOptions(msg);
+    }
+    @GetMapping(value = "/chat/Stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> chatStream(@RequestParam String msg){
+        return service.chatOptionsWithFlux(msg);
     }
 }
--- src/main/java/com/bhargav/ai/demo/service/AIChatService.java
+++ src/main/java/com/bhargav/ai/demo/service/AIChatService.java
@@ -1,20 +1,50 @@
 package com.bhargav.ai.demo.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @Service
 public class AIChatService {
 
-    private final ChatClient chatClient;
+  private final ChatClient chatClient;
 
-    public AIChatService (ChatClient.Builder chatClientBuilder){
-        this.chatClient = chatClientBuilder.build();
-    }
+  public AIChatService(ChatClient.Builder chatClientBuilder) {
+    this.chatClient = chatClientBuilder.build();
+  }
 
-    public String chatWithOllama(String message){
-        return chatClient.prompt(message)
-                .call()
-                .content();
-    }
+  public String chatWithOllama(String message) {
+    return chatClient.prompt(message).call().content();
+  }
+
+  public String chatOptions(String msg) {
+    ChatOptions options =
+        ChatOptions.builder()
+            .temperature(0.3)
+            //                .maxTokens(10)
+            .frequencyPenalty(0.7)
+            .presencePenalty(0.7)
+            .stopSequences(List.of("}"))
+            .topK(50)
+            .topP(0.5)
+            .build();
+    return chatClient.prompt(msg).options(options).call().content();
+  }
+
+  public Flux<String> chatOptionsWithFlux(String msg) {
+    ChatOptions options =
+        ChatOptions.builder()
+            .temperature(0.3)
+            //                .maxTokens(10)
+            .frequencyPenalty(0.7)
+            .presencePenalty(0.7)
+            .stopSequences(List.of("}"))
+            .topK(50)
+            .topP(0.5)
+            .build();
+    return chatClient.prompt(msg).options(options).stream().content();
+  }
 }
